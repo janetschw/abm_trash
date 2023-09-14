f1ee519238ac19dace1813cdc2c7d1243739ca17
kabembler <135595338+kabembler@users.noreply.github.com>
2023-09-14 21:20:19 +0200

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,6 +1,8 @@
 from mesa import Agent
 from mesa.model import Model 
 
+# Was ist der Müll
+
 class TrashAgent (Agent):
     def __init__(self, unique_id: int, model: Model) -> None:
         super().__init__(unique_id, model)
