6533a22948cf9567435bbaf55c386e91e6978fba
kabembler <135595338+kabembler@users.noreply.github.com>
2023-09-14 21:48:25 +0200

    Update trash_bin.py
    Hashtags hinzugefügt

--- trash_bin.py
+++ trash_bin.py
@@ -2,15 +2,20 @@ from mesa import Agent
 from mesa.model import Model
 from trash import TrashAgent
 
+# Hier wird der mülleimer (TrashbinAgent) definiert. 
+# Seine Eigenschaften werden hinzugefügt (self.)
+
 class TrashBinAgent (Agent):
     def __init__(self, unique_id: int, model: Model, capacity: int, pickup_interval: int, pos) -> None:
         super().__init__(unique_id, model)
-        self.pos = pos
+        self.pos = pos # Postion
         self.trash = self.random.randint(0, capacity)
-        self.capacity = capacity
-        self.pickup_interval = pickup_interval
-        self.steps_since_last_pickup = 0
+        self.capacity = capacity # Füllmenge
+        self.pickup_interval = pickup_interval # Wie oft wird der Müll geleert
+        self.steps_since_last_pickup = 0 # Wie lange ist die letzte entleerung her?
 
+    # Was "denkt" der Mülleimer bei jedem schritt in welcher Reihenfolge
+    
     def step(self):
         if self.trash > self.capacity:
             self.drop_trash()
@@ -18,13 +23,16 @@ class TrashBinAgent (Agent):
         if self.steps_since_last_pickup > self.pickup_interval:
             self.dispose_trash()
 
+    # Wann lässt der Mülleimer Müll in den Felder um sich herrum fallen? 
     def drop_trash(self):
-        excess_trash = self.trash - self.capacity
+        excess_trash = self.trash - self.capacity #wenn er voll ist 
         for i in range(excess_trash):
             new_trash = TrashAgent(model=self.model, unique_id=self.model.next_id())
             pos = self.random.choice(self.model.grid.get_neighborhood(self.pos, True, False, 1))
             self.model.grid.place_agent(new_trash, pos)
             self.model.total_trash += 1
+            
+        # Müll wird zum Grid hinzugefügt
         
         self.trash = self.capacity
 
